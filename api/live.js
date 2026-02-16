4adc0ff3355403f1caa960f4c20dcc97f460828f
nalinherath <nalinherath621@gmail.com>
2026-02-17 00:28:46 +0530

    Update live.js

--- api/live.js
+++ api/live.js
@@ -1,14 +1,18 @@
 export default async function handler(req, res) {
+    try {
+        const response = await fetch("https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live", {
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key": "b0d9720e6bmsh92fbc195fcfa86ap1ab0afjsnf17af6585b48",
+                "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
+            }
+        });
 
-const response = await fetch("https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live", {
-method: "GET",
-headers: {
-"X-RapidAPI-Key": "b0d9720e6bmsh92fbc195fcfa86ap1ab0afjsnf17af6585b48",
-"X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
-}
-});
+        const data = await response.json();
 
-const data = await response.json();
-
-res.status(200).json(data);
+        res.status(200).json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to fetch matches" });
+    }
 }
