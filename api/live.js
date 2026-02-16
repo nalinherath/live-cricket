72a86aec76afe868d5882142d1e3063238ceb945
nalinherath <nalinherath621@gmail.com>
2026-02-17 00:08:56 +0530

    Create live.js

---
+++ api/live.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+
+const response = await fetch("https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live", {
+method: "GET",
+headers: {
+"X-RapidAPI-Key": "b0d9720e6bmsh92fbc195fcfa86ap1ab0afjsnf17af6585b48",
+"X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
+}
+});
+
+const data = await response.json();
+
+res.status(200).json(data);
+}
